9e081ea6a5f44d4fb0916a8f0ab0cba568377eba
Woody Chang <kazettique@gmail.com>
2023-10-30 21:43:22 +0800

    eslint, prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'eslint-config-next',
+    'plugin:typescript-sort-keys/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', 'simple-import-sort', 'sort-keys'],
+  rules: {
+    'prettier/prettier': 'error',
+    semi: 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-keys': ['off'], // disable default eslint sort-keys
+    'sort-keys/sort-keys-fix': ['error', 'asc', { caseSensitive: true, minKeys: 2, natural: true }], // use sort-keys-fix for fixing
+  },
+};
